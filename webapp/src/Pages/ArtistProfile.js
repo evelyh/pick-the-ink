14f1d8f6c307361dd65e03683aff8d5d9f79d54f
Sean-1005 <66577352+Sean-1005@users.noreply.github.com>
2022-03-01 13:51:44 -0500

    Artists Profile and Artists Gallery

--- webapp/src/Pages/ArtistProfile.js
+++ webapp/src/Pages/ArtistProfile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header } from '../components/Header'
+import { Header } from '../components/header'
 import { Container } from 'react-bootstrap'
 import profilePic from '../assets/img/profilepic.jpg'
 import '../assets/css/userProfile.css'
@@ -16,6 +16,10 @@ function ArtistProfile() {
       email: "spongebob@gmail.com",
       birthday: "1999-12-13",
       phoneNum : "1234567890",
+      following: 309,
+      followers: 622,
+      homeLocation: "Bikini Bottom",
+      artStyle: ["Blackwork", "Watercolor"], 
       style: ["Blackwork", "Watercolor"],
       image: "../images/profilepic.jpg"
     });
@@ -69,6 +73,28 @@ function ArtistProfile() {
                 <label className="col-sm-3 col-form-label col-form-label">Phone:</label>
                 <div className="col-sm-7">
                   <label className="col-sm-6 col-form-label col-form-label">{values.phoneNum}</label>
+                </div>
+
+                <label className="col-sm-3 col-form-label col-form-label">Following:</label>
+                <div className="col-sm-7">
+                  <label className="col-sm-6 col-form-label col-form-label">{values.following}</label>
+                </div>
+
+                <label className="col-sm-3 col-form-label col-form-label">Followers:</label>
+                <div className="col-sm-7">
+                  <label className="col-sm-6 col-form-label col-form-label">{values.followers}</label>
+                </div>
+
+                <label className="col-sm-3 col-form-label col-form-label">Home Location:</label>
+                <div className="col-sm-7">
+                  <label className="col-sm-6 col-form-label col-form-label">{values.homeLocation}</label>
+                </div>
+
+                <label className="col-sm-3 col-form-label col-form-label">My art styles:</label>
+                <div className="col-sm-7">
+                  {values.style.map((_, index) => (
+                    <li className="col-sm-6 col-form-label col-form-label" key={index}>{values.artStyle[index]}</li>
+                  ))}
                 </div>
 
                 <label className="col-sm-3 col-form-label col-form-label">Favorite styles:</label>
