be2efcd18d69d8d2601ad6080d8e064312059af9
xinpeng13 <x.shan@mail.utoronto.ca>
2022-03-01 20:48:45 -0500

    Update

--- webapp/src/Pages/ArtistGallery.js
+++ webapp/src/Pages/ArtistGallery.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header } from '../components/header'
+import { Header } from '../components/Header'
 import { Container } from 'react-bootstrap'
 import profilePic from '../assets/img/profilepic.jpg'
 import pic1 from '../assets/img/gallery_pic1.jpg'
--- webapp/src/Pages/UserProfile.js
+++ webapp/src/Pages/UserProfile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header } from '../components/header'
+import { Header } from '../components/Header'
 import { Container } from 'react-bootstrap'
 import profilePic from '../assets/img/profilepic.jpg'
 import '../assets/css/userProfile.css'
