13289638601860fc779c46f67b153c476499e653
Eve Lei <evelyh.1217@gmail.com>
2022-03-06 13:00:12 -0500

    styling problem

--- webapp/src/Pages/LandingPage.js
+++ webapp/src/Pages/LandingPage.js
@@ -1,72 +1,98 @@
-import React from 'react'
-import { Header } from '../components/header'
+import React, { Component } from 'react'
+import { Header } from '../components/Header.js'
 import { Container } from 'react-bootstrap'
 import { Button, Form} from 'react-bootstrap'
+import {Multiselect} from 'multiselect-react-dropdown';
 import { CountryDropdown, RegionDropdown} from 'react-country-region-selector';
+import ArtistCard from 'components/ArtistCard.js';
 import "../assets/css/landingPage.css";
 
-function LandingPage() {
+export class LandingPage extends Component {
 
-  var country;
-  var region;
+  state = {
+    country: '',
+    region: '',
+    start: '',
+    end: '',
+    styles: [],
+    }
 
-  const selectCountry = (val) => {
-    country = val;
-  }
+  optionData = [{name:'Traditional', id: 1}, 
+    {name:'Japanese', id: 2}, 
+    {name:'New School', id: 3},
+    {name:'Realism', id: 5}, 
+    {name:'Illustrative', id: 6}, 
+    {name: 'Portraiture', id: 7}, 
+    {name: 'Blackwork', id: 8},
+    {name: 'Watercolor', id: 9},
+    {name:'Lettering',id: 10},
+    {name:'Geometric',id: 11},
+    {name:'Surrealism',id: 12},
+    {name:'Microrealism',id: 13},
+    {name:'Minimalism',id: 14},
+    {name:'Single Line',id: 14},
+    {name:'Dot Work',id: 14},];
 
-  const selectRegion = (val) => {
-    region = val;
-  }
-
+  render(){
     return (
       <div>
         <div><Header/> </div>
         <Container>
-              <h4 className="mb-3">Add filter to find the right artists for you </h4>
+              <h4 className="mb-4">Welcome! Get started by finding the right Artist. </h4>
             <Form>
               <div className='row'>
-                <div className='col-5 mb-3' >                 
-                <h7> Country: </h7>
-                  <CountryDropdown
-                        value={country}
-                        onChange={(val) => selectCountry(val)}/> 
-                </div>
-                <div className='col-5 mb-3'>
-                 <h7> Region: </h7>
-                  <RegionDropdown
-                      country={country}
-                      value={region}
-                      onChange={(val) => selectRegion(val)}/>
-                </div>
+                <Form.Group className='col-5 mb-2' >                 
+                <Form.Label> Country: </Form.Label>
+                <CountryDropdown
+                        className='countrySelector'
+                        value={this.state.country}
+                        onChange={(val) => this.setState({country: val})}></CountryDropdown> 
+                </Form.Group>
+                <Form.Group className='col-5 mb-2'>
+                 <Form.Label> Region: </Form.Label>
+                <RegionDropdown
+                      className='countrySelector'
+                      country={this.state.country}
+                      value={this.state.region}
+                      onChange={(val) => this.setState({region: val})}/>
+                </Form.Group>
               </div>
                 <div className = 'row'>
-                  <Form.Group className="mb-3 col-3" >
+                  <Form.Group className="mb-2 col-3" >
                       <Form.Label>Start:</Form.Label>
                       <Form.Control type="date"/>
                   </Form.Group>
 
-                  <Form.Group className="mb-3 col-3" >
+                  <Form.Group className="mb-2 col-3" >
                       <Form.Label>End:</Form.Label>
                       <Form.Control type="date"/>
                   </Form.Group>
                   </div>
 
-                  <Form.Group className="mb-3" >
+                  <Form.Group className="mb-2 col-5" >
                       <Form.Label>Styles:</Form.Label>
-                      {["Watercolor", "Fine line", "Cartoon"].map((label) => (
-                      <Form.Check type='radio' label={label}/>))}
+                      <Multiselect options={this.optionData} displayValue="name"
+                        onSelect={(val) => this.setState({styles: this.state.styles.concat(val)})}
+                        onRemove={(val) => this.setState({styles: this.state.styles.remove(val)})}>
+                      </Multiselect>
                   </Form.Group>
 
-                  <Button id="button-16" type="submit" >Apply</Button>
+                  <Button className="mt-3 mb-3" id="button-14" type="submit" >Apply</Button>
               </Form>
           </Container>
           <Container>
             <div className='cardContainer'> 
-            
+              <ArtistCard/>
+              <ArtistCard/>
+              <ArtistCard/>
+              <ArtistCard/>
+              <ArtistCard/>
+              <ArtistCard/>
             </div>
           </Container>
       </div>
-    )
+      )
+    }
 
 }
 
--- webapp/src/components/ArtistCard.js
+++ webapp/src/components/ArtistCard.js
@@ -1,18 +1,36 @@
-import React, { useState } from 'react'
+import React, { Component, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Button, Form} from 'react-bootstrap'
-import { CountryDropdown, RegionDropdown} from 'react-country-region-selector';
+import ReactDOM from 'react-dom';
+import pic1 from '../assets/img/gallery_pic1.jpg'
+import pic2 from '../assets/img/gallery_pic2.jpg'
+import pic3 from '../assets/img/squidward.jpg'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import "../assets/css/artistCard.css";
 
-function ArtistCard(props){
-
-        return(
+export class ArtistCard extends Component{
+  render(){
+    return(
         <div class='card'>
-            
+                <h5> @PatrickYahh </h5>
+                <h7> Hi This is a tattooist based in Toronto </h7>
+            <div class='carousel'>
+                <Carousel>
+                <div>
+                        <img src={pic1} />
+                </div>
+                <div>
+                        <img src={pic2} />
+                </div>
+                <div>
+                        <img src={pic3} />
+                </div>
+                </Carousel>
+            </div>
+
         </div>
-        )
-
-
+  )
+}
 }
 
 export default ArtistCard
