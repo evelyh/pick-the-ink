d94451cba83508a364d10d6118304b43cbbe5927
Sean-1005 <66577352+Sean-1005@users.noreply.github.com>
2022-03-06 12:38:05 -0500

    add, edit, delete from gallery

--- webapp/src/Pages/ArtistGallery.js
+++ webapp/src/Pages/ArtistGallery.js
@@ -6,60 +6,124 @@ import pic1 from '../assets/img/gallery_pic1.jpg'
 import pic2 from '../assets/img/gallery_pic2.jpg'
 import '../assets/css/userProfile.css'
 import '../assets/css/artistsGallery.css'
+import { VscAdd } from "react-icons/vsc";
+import { IconContext } from "react-icons";
 import PopUpAppointmentForm from '../components/PopUpAppointmentForm'
+import PopUpEditGallery from '../components/PopUpEditGallery'
+import PopUpDelGallery from "../components/PopUpDelGallery"
+import PopUpAddGallery from "../components/PopUpAddGallery"
 import { useState } from 'react'
-
+import { Card, CardImg, CardBody, CardTitle, CardText, Button } from 'reactstrap';
+import FlatList from 'flatlist-react';
 
 function ArtistGallery() {
 
-    const [values, setValues] = useState({
-      firstName: "Sponge",
-      lastName: "Bob",
-      userName: "Spongebob",
-      email: "spongebob@gmail.com",
-      birthday: "1999-12-13",
-      phoneNum : "1234567890",
-      following: 309,
-      followers: 622,
-      homeLocation: "Bikini Bottom",
-      artStyle: ["Blackwork", "Watercolor"], 
-      style: ["Blackwork", "Watercolor"],
-      image: "../images/profilepic.jpg",
-      hasShadow: false,
-    });
+  const [values, setValues] = useState({
+    userName: "Spongebob",
+    email: "spongebob@gmail.com",
+    following: 309,
+    followers: 622,
+    homeLocation: "Bikini Bottom",
+    image: "../images/profilepic.jpg",
+    gallery:[
+      {id:0, title: "My BF", description: "Patrick and me", img: pic1},
+      {id:1, title: "Great time", description: "Patrick and me", img: pic2}],
+  });
 
-    const [buttonPopUp, setButtonPopUp] = useState(false);
+  const [buttonPopUpEdit, setButtonPopUpEdit] = useState(undefined);
+  const [buttonPopUpDel, setButtonPopUpDel] = useState(undefined);
+  const [buttonPopUpAdd, setButtonPopUpAdd] = useState(false);
 
+  const deleteById = (id) => {
+    setValues({gallery:values.gallery.filter((item)=>{
+        return item.id !== id
+    })})
+  }
+
+  const addNewPic = (newPic) => {
+    const newGallery = values.gallery.concat(newPic);
+    setValues({gallery: newGallery});
+  }
+
+  const renderItem = (galleryPic, index) => {
+
+    return(
+      <Card key={galleryPic.id} style={{width: '20rem'}}>
+        <CardImg className='galleryPics' top src={galleryPic.img} alt="..."/>
+        <CardBody>
+        <CardTitle className='cardTitle'>{galleryPic.title}</CardTitle>
+        <CardText className='cardText'>{galleryPic.description}</CardText>
+          <PopUpEditGallery 
+            values={values}
+            title = {galleryPic.title}
+            index = {index}
+            description = {galleryPic.description}
+            setValues = {setValues}
+            trigger={buttonPopUpEdit} 
+            setTrigger={setButtonPopUpEdit}
+            >
+              Edit 
+          </PopUpEditGallery> 
+          <PopUpDelGallery 
+            values={values}
+            index = {index}
+            id = {galleryPic.id}
+            setValues = {setValues}
+            onDelete = {deleteById}
+            trigger={buttonPopUpDel} 
+            setTrigger={setButtonPopUpDel}
+            >
+              Del
+          </PopUpDelGallery> 
+            <Button id="edit" className="btn-round btn-icon" color="primary" size='sm' onClick={()=> setButtonPopUpEdit(index)}>Edit</Button>
+            <Button id="del" className="btn-round btn-icon" color="danger" size='sm' onClick={()=> setButtonPopUpDel(index)}>Delete</Button>
+        </CardBody>
+      </Card>
+    );
+  }
 
     return (
       <div>
         <div>
           <Header/>
         </div>
-        <button id='button-addPic' onClick={()=> setButtonPopUp(true)}>+</button>
-
-        <PopUpAppointmentForm info={values} setInfo = {setValues} trigger={buttonPopUp} setTrigger={setButtonPopUp}>My Popup</PopUpAppointmentForm>
+        {/* <button id='button-addPic' onClick={()=> setButtonPopUp(true)}>+</button> */}
+  
+        {/* <PopUpAppointmentForm info={values} setInfo = {setValues} trigger={buttonPopUp} setTrigger={setButtonPopUp}>My Popup</PopUpAppointmentForm> */}
         <div className="container">
+          <PopUpAddGallery
+            values={values}
+            setValues = {setValues}
+            id = {values.gallery.length+1}
+            onAdd = {addNewPic}
+            trigger={buttonPopUpAdd} 
+            setTrigger={setButtonPopUpAdd}
+          >Add</PopUpAddGallery>
+          <Button id="button-addPic" className="btn-round btn-icon" color="primary" onClick={()=> setButtonPopUpAdd(true)}>
+            <IconContext.Provider value={{ color: 'white', size: '15px' }}><VscAdd/> Add </IconContext.Provider>
+          </Button>
           <div className="row">
             <div className="col-3">
               <Container >
                 <img src={profilePic} id="profileCirclePic"  alt='profile'  />
-                <button id='button-appointment' onClick={()=> setButtonPopUp(true)}>Book an appointment</button>
-                <label className='infoLine'>Username: {values.userName}</label>
-                <label className='infoLine'>Location: {values.homeLocation}</label>
+                {/* <button id='button-appointment' onClick={()=> setButtonPopUp(true)}>Book an appointment</button> */}
               </Container>
             </div>
             <div className='col-7'>
               <div className='header'>
                 <h3>My Gallery</h3>
               </div>
-              <img src={pic1} className = "galleryPics"  alt='profile'  />
-              <img src={pic2} className = "galleryPics"  alt='profile'  />
+              <ul>
+              <FlatList
+                list={values.gallery}
+                renderItem={renderItem}
+              />
+              </ul>
             </div>
           </div>
         </div>
       </div>
     )
-  }
+}
 
 export default ArtistGallery
---
+++ webapp/src/components/PopUpEditGallery.js
@@ -0,0 +1,79 @@
+import { React, useState }  from 'react';
+import '../assets/css/artistsGallery.css';
+import { useForm } from "react-hook-form";
+import {
+    FormGroup,
+    Label,
+    Input,
+    Button,
+    Modal
+  } from "reactstrap";
+
+
+function PopUpEditGallery(props) {
+
+    const { handleSubmit } = useForm();
+
+    const [prev] = useState({
+        userName: props.values.userName,
+        email: props.values.email,
+        following: props.values.following,
+        followers: props.values.followers,
+        homeLocation: props.values.homeLocation,
+        image: props.values.image,
+        gallery:props.values.gallery,
+      });
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        console.log(data);
+    }
+
+    const onChange = (val, index, field) =>{
+        let copy = {...props.values.gallery};
+        copy[index][field] = val;
+        props.setValues({gallery: copy})
+    } 
+
+    return (props.trigger != undefined) ? (
+        props.trigger == props.index && 
+        <div className='popup'>
+            <div className='popupInner'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <FormGroup>
+                        <Label>Title</Label>
+                        <Input
+                        type="text"
+                        placeholder="Enter title"
+                        value = {props.title}
+                        onChange={val => onChange(val.target.value, props.index, "title")}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Desription</Label>
+                        <br/>
+                        <textarea
+                        className='textArea'
+                        type="text"
+                        placeholder ="Enter description"
+                        value = {props.description}
+                        onChange={val => onChange(val.target.value, props.index, "description")}
+                        />
+                    </FormGroup>
+                    <Button className="btn-round btn-icon" color="success" size='sm' onClick={()=>props.setTrigger(undefined)}>
+                        Submit
+                    </Button>
+                    <Button id = "del_but" className="btn-round btn-icon" color="default" size='sm'
+                    onClick={()=>{
+                        props.setValues({...prev});
+                        props.setTrigger(undefined);}}>
+                        Cancel
+                    </Button>
+                </form>
+            </div>
+        </div>
+    ): "";
+}
+
+export default PopUpEditGallery;
