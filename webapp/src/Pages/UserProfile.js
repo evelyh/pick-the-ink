6c862030618180de54495bf3801e008941aabf85
Sean-1005 <66577352+Sean-1005@users.noreply.github.com>
2022-03-06 12:42:05 -0500

    resolve UserProfile conflicts

--- webapp/src/Pages/UserProfile.js
+++ webapp/src/Pages/UserProfile.js
@@ -23,12 +23,10 @@ function UserProfile() {
       artStyle: ["Blackwork", "Watercolor"], 
       style: ["Blackwork", "Watercolor"],
       image: "../images/profilepic.jpg",
-      isAritist:true,
     });
 
     const [buttonPopUp, setButtonPopUp] = useState(false);
 
-    if(values.isAritist){
       return (
         <div>
           <div>
@@ -113,69 +111,6 @@ function UserProfile() {
           </div>
         </div>
       )
-    }
-    else
-    {return (
-      <div>
-        <div>
-          <Header/>
-        </div>
-        <PopUpProfileForm info={values} setInfo = {setValues} trigger={buttonPopUp} setTrigger={setButtonPopUp}>My Popup</PopUpProfileForm>
-        <div className="container">
-          <div className="row">
-            <div className="col-3">
-              <Card>
-              <img src={profilePic} id="profileCirclePic" alt='profile' />
-              </Card>
-            </div>
-            <div className="col-7">
-              <Container id="profileContainer">
-              <div className="row mb-3">
-                
-                <label className="col-sm-3 col-form-label col-form-label">Username:</label>
-                <div className="col-sm-7">
-                  <label className="col-sm-6 col-form-label col-form-label">{values.userName}</label>
-                </div>
-
-                <label className="col-sm-3 col-form-label col-form-label">First name:</label>
-                <div className="col-sm-7">
-                  <label className="col-sm-6 col-form-label col-form-label">{values.firstName}</label>
-                </div>
-
-                <label className="col-sm-3 col-form-label col-form-label">Last name:</label>
-                <div className="col-sm-7">
-                  <label className="col-sm-6 col-form-label col-form-label">{values.lastName}</label>
-                </div>
-                
-                <label className="col-sm-3 col-form-label col-form-label">Email:</label>
-                <div className="col-sm-7">
-                  <label className="col-sm-6 col-form-label col-form-label">{values.email}</label>
-                </div>
-
-                <label className="col-sm-3 col-form-label col-form-label">Date of Birth:</label>
-                <div className="col-sm-7">
-                  <label className="col-sm-6 col-form-label col-form-label">{values.birthday}</label>
-                </div>
-
-                <label className="col-sm-3 col-form-label col-form-label">Phone:</label>
-                <div className="col-sm-7">
-                  <label className="col-sm-6 col-form-label col-form-label">{values.phoneNum}</label>
-                </div>
-
-                <label className="col-sm-3 col-form-label col-form-label">Favorite styles:</label>
-                <div className="col-sm-7">
-                  {values.style.map((_, index) => (
-                    <li className="col-6 col-form-label col-form-label" key={index}>{values.style[index]}</li>
-                  ))}
-                </div>
-              </div>
-              <button id='button-16' onClick={()=> setButtonPopUp(true)}>Edit your profile</button>
-              </Container>
-            </div>
-          </div>
-        </div>
-      </div>
-    )}
   }
 
 export default UserProfile
