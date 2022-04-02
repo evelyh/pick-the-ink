958868dfca3b03945382043bfcb91877dba1a26e
Sean-1005 <66577352+Sean-1005@users.noreply.github.com>
2022-04-02 00:16:29 -0400

    Finished all the basic functionalities of gallery

--- webapp/src/components/PopUpDelGallery.js
+++ webapp/src/components/PopUpDelGallery.js
@@ -1,13 +1,42 @@
 import { React }  from 'react';
 import '../assets/css/artistsGallery.css';
 import { Button } from "reactstrap";
-import _ from "lodash"
+import { getUser } from "../apiHook/profile";
+import { updateArtistsGallery, deleteImageById } from "../apiHook/image";
 
 
 export default function PopUpDelGallery(props){
 
+    const deleteById = async (id) => {
+
+        await getUser(props.myID).then(async (res) => {
+          console.log(id)
+          console.log(res)
+          var idx = -1;
+          for (var i = 0; i < res.artistSub.artworks.length; i++)
+          {
+            if(res.artistSub.artworks[i] === id)
+            {
+              idx = i;
+              break;
+            }
+          }
+          if (idx !== -1){
+            res.artistSub.artworks.splice(idx, 1);
+            await updateArtistsGallery(res).catch(error => {
+              console.log(error);
+            });
+    
+            await deleteImageById(id).catch(error => {
+              console.log(error);
+            });
+          }
+        });
+        window.location.reload();
+    }
+
     return (props.trigger !== undefined) ? (
-        props.trigger === props.index && 
+        props.trigger === props.id && 
         <div id="popupDel" className="popup">
             <div className='popupInner'>
                 <h5>Delete Picture</h5>
@@ -16,7 +45,7 @@ export default function PopUpDelGallery(props){
                     onClick={(event)=>{
                             event.preventDefault();
                             props.setTrigger(undefined);
-                            props.onDelete(props.index, event);}}>
+                            deleteById(props.id);}}>
                     Delete
                 </Button>
                 <Button id="canl_bt" className="btn-round btn-icon" color="default" size='sm' 
